2c1f449b483160f252646c0605f6494c29843084
tintinweb <tintinweb@oststrom.com>
2019-03-29 23:21:01 +0100

    use cancellationToken instead of backoff time only clear and re-import issues from files that have changed

--- src/features/genericDiag.js
+++ src/features/genericDiag.js
@@ -38,18 +38,6 @@ class DiliDiagnosticCollection {
         this.running = 0;
     }
 
-    setRunning(timeout){
-        this.running = Date.now() + timeout
-    }
-
-    isRunning(){
-        if(Date.now()>this.running){
-            this.running=0
-            return false
-        }
-        return true
-    }
-
     newCollection(name){
         if(!this.collections.hasOwnProperty(name)){
             this.collections[name] = vscode.languages.createDiagnosticCollection(name)
@@ -68,72 +56,74 @@ class DiliDiagnosticCollection {
         },this)
     }
 
-    async updateIssues(){
+    async updateIssues(cancellationToken){
         return new Promise((reject, resolve) => {
             var that = this;
-            if(this.isRunning()){
-                reject("is still running")
-                return;
-            }
-            this.setRunning(60)
-            this.clearAll()
-            this.issueFileGlob.forEach(function(g){
-            glob(path.join(this.base,g), {}, function (er, files) {
-                // files is an array of filenames.
-                // If the `nonull` option is set, and nothing
-                // was found, then files is ["**/*.js"]
-                // er is an error object or null.
-                files.forEach(function(f){
-                    let basedir = path.dirname(f)
-                    let collectionName = path.basename(f)
-                    let collection = that.newCollection(collectionName)
-                    try {
-                        let content = fs.readFileSync(f)
-                        if(!fileDidChange(f, content)){
-                            return
-                        }
-                        var issues = JSON.parse(content);
-                        /*
-                            {"onInputFile": "contracts/BountiesMetaTxRelayer.sol", 
-                            "atLineNr": "10", 
-                            "ruleType": "code_smell", 
-                            "severity": "major", 
-                            "remediationEffortMinutes": "5",
-                            "linterVersion": "0.1", 
-                            "linterName": "maru", 
-                            "message": "State Variable  Default Visibility - It is best practice to set the visibility of state variables explicitly. The default           visibility for \"bountiesContract\" is internal. Other possible visibility values are public and private.",         
-                            "forRule": "State_Variable_Default_Visibility"}
-                        */
-                        issues.forEach(function(issue){
-                            //abspath or relpath?
-                            let targetFileUri = issue.onInputFile.startsWith("/") ? issue.onInputFile : vscode.Uri.file(path.join(basedir,issue.onInputFile))
-                            if(!fs.existsSync(targetFileUri.path)){
-                                // skip nonexistent files
-                                // todo: maybe skip node_modules?
-                                return
+            try{
+                this.issueFileGlob.forEach(function(g){
+                    glob(path.join(this.base,g), {}, function (er, files) {
+                        // files is an array of filenames.
+                        // If the `nonull` option is set, and nothing
+                        // was found, then files is ["**/*.js"]
+                        // er is an error object or null.
+                        files.forEach(function(f){
+                            if(cancellationToken.isCancellationRequested){
+                                throw cancellationToken
                             }
-                            collection.set(targetFileUri, [{
-                                code: '',
-                                message: `${issue.linterName}/${issue.severity}/${issue.ruleType} - ${issue.message}`,
-                                range: new vscode.Range(new vscode.Position(issue.atLineNr - 1, 0), new vscode.Position(issue.atLineNr - 1, 255)),
-                                severity: toVscodeSeverity[issue.severity],
-                                source: "",
-                                relatedInformation: []
-                            }]);
+                            let basedir = path.dirname(f)
+                            let collectionName = f //path.basename(f)
+                            let collection = that.newCollection(collectionName)
+                            try {
+                                let content = fs.readFileSync(f)
+                                if(!fileDidChange(f, content)){
+                                    return
+                                }
+                                collection.clear()  //only reload this collection
+                                var issues = JSON.parse(content);
+                                /*
+                                    {"onInputFile": "contracts/BountiesMetaTxRelayer.sol", 
+                                    "atLineNr": "10", 
+                                    "ruleType": "code_smell", 
+                                    "severity": "major", 
+                                    "remediationEffortMinutes": "5",
+                                    "linterVersion": "0.1", 
+                                    "linterName": "maru", 
+                                    "message": "State Variable  Default Visibility - It is best practice to set the visibility of state variables explicitly. The default           visibility for \"bountiesContract\" is internal. Other possible visibility values are public and private.",         
+                                    "forRule": "State_Variable_Default_Visibility"}
+                                */
+                                issues.forEach(function(issue){
+                                    //abspath or relpath?
+                                    let targetFileUri = issue.onInputFile.startsWith("/") ? issue.onInputFile : vscode.Uri.file(path.join(basedir,issue.onInputFile))
+                                    if(!fs.existsSync(targetFileUri.path)){
+                                        // skip nonexistent files
+                                        // todo: maybe skip node_modules?
+                                        return
+                                    }
+                                    collection.set(targetFileUri, [{
+                                        code: '',
+                                        message: `${issue.linterName}/${issue.severity}/${issue.ruleType} - ${issue.message}`,
+                                        range: new vscode.Range(new vscode.Position(issue.atLineNr - 1, 0), new vscode.Position(issue.atLineNr - 1, 255)),
+                                        severity: toVscodeSeverity[issue.severity],
+                                        source: "",
+                                        relatedInformation: []
+                                    }]);
+                                })
+                            } catch (err) {
+                                console.error(err)
+                            }
                         })
-                    } catch (err) {
-                        console.error(err)
-                    }
-                })
-            })
-        }, this)
+                    resolve()
+                    })
+                }, this)
+            } catch (err) {
+                reject(err)
+                if (typeof err !=="CancellationToken"){
+                    throw err
+                }
+            }
         })
     }
 }
-
-/*
-diagnosticCollections.compiler.set(
-*/
 
 module.exports = {
     DiliDiagnosticCollection:DiliDiagnosticCollection
