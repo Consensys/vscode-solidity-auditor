789fbc58b2b6f797d20a983afa82ce3454ee29e7
tintinweb <tintinweb@oststrom.com>
2019-05-19 13:38:04 +0200

    add missing settings file

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+/** 
+ * @author github.com/tintinweb
+ * @license MIT
+ * 
+ * 
+ * */
+/** globals - const */
+const languageId = "solidity";
+const docSelector = {
+    language: languageId,
+    scheme: 'file',
+}
+
+module.exports = {
+    languageId:languageId,
+    docSelector:docSelector
+}
